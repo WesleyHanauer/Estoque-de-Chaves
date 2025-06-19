251c2d3755c68953f74e8801b5883af955cf8eaf
WesleyHanauer <wesleyhanauer@gmail.com>
2025-06-19 18:56:30 -0300

    Objects are being saved and loaded successfully

--- Estoque de Chaves/main/main.js
+++ Estoque de Chaves/main/main.js
@@ -1,7 +1,11 @@
 class chave{
-    constructor(nome, quantidade){
+    constructor(id, nome, quantidade){
+        this.id=id
         this.nome=nome
         this.quantidade=quantidade
+    }
+    get getId(){
+        return this.id
     }
     get getNome(){
         return this.nome
@@ -11,39 +15,35 @@ class chave{
     }
 }
 
-const form = document.getElementById("newChaveForm");
+console.log(localStorage.length)
+
+const form = document.getElementById("newChaveForm")
+
+if(localStorage.getItem(0)!=null){
+    const storedData = []
+    const loadedChave = []
+    for (let i = 0; localStorage.getItem(i) != null; i++) {
+        storedData[i] = localStorage.getItem(i);
+        loadedChave[i] = storedData[i] ? JSON.parse(storedData[i]) : {};
+    }
+    console.log(loadedChave)
+}
 
 form.addEventListener("submit", function e(event){
     event.preventDefault()
-
-    const newChaveNome = document.getElementById("nomeForm").value
-    const newChaveQuantidade = document.getElementById("quantidadeForm").value
-    const newChave = new chave(newChaveNome, newChaveQuantidade)
-
+    const newChaveId = 0
+    for (let i = 0; localStorage.getItem(i) != null; i++) {
+        if(i > 0 && localStorage.getItem(i)==null){
+            newChaveId = i-1
+        }
+    }
+    const newChave = createChave(newChaveId, document.getElementById("nomeForm").value, document.getElementById("quantidadeForm").value)
     const stringChave = JSON.stringify(newChave)
-
-    localStorage.setItem("chaves", stringChave)
-
-    const storedData = localStorage.getItem("chaves");
-    const loadedStock = storedData ? JSON.parse(storedData) : {};
-
-    console.log(loadedStock)
+    localStorage.setItem(newChaveId, stringChave)
 });
 
-/*function createChave(){
-    const newChaveNome = document.getElementById("nomeForm").value
-    const newChaveQuantidade = document.getElementById("quantidadeForm").value
-
-    const newChave = new chave(newChaveNome, newChaveQuantidade)
-
-    console.log(chaveForm)
-
-    return newChave
-}*/
-
-
-function readChave(chave){
-    return this.chave
+function createChave(id, newChaveNome, newChaveQuantidade){
+    return newChave = new chave(id, newChaveNome, newChaveQuantidade)
 }
 
 function updateChave(){
@@ -53,3 +53,6 @@ function updateChave(){
 function deleteChave(){
 
 }
+
+    //const storedData = localStorage.getItem("chaves");
+    //const loadedStock = storedData ? JSON.parse(storedData) : {};
