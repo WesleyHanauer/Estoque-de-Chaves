514aef288aff39e2a63c75289fa9f851e3204892
WesleyHanauer <wesleyhanauer@gmail.com>
2025-06-22 17:14:49 -0300

    Created the display table and new objects are shown on screen

--- script/main.js
+++ script/main.js
@@ -15,27 +15,60 @@ class chave{
     }
 }
 
-console.log(localStorage.getItem(5))
-
+//localStorage.clear()
 console.log(localStorage.length)
 
-const form = document.getElementById("newChaveForm")
+const table = document.createElement("table")
+const headRow = document.createElement("tr")
+const headCol1 = document.createElement("th")
+const headCol2 = document.createElement("th")
+headRow.setAttribute("colspan", "2");
+headCol1.innerText = "Quantidade"
+headCol2.innerText = "Chaves"
+headRow.appendChild(headCol1)
+headRow.appendChild(headCol2)
 
 if(localStorage.getItem(0)!=null){
     for (let i = 0; localStorage.getItem(i) != null; i++) {
         const storedData = localStorage.getItem(i);
-        const loadedChave = storedData ? JSON.parse(storedData) : {};
-        console.log(loadedChave)
+        const loadedChaveString = (storedData ? JSON.parse(storedData) : {})
+        const loadedChave = new chave(loadedChaveString.id, loadedChaveString.nome, loadedChaveString.quantidade)
+        const bodyRow = document.createElement("tr")
+        const bodyCol1 = document.createElement("th")
+        const bodyCol2 = document.createElement("th")
+        bodyCol1.innerText = loadedChave.getNome
+        bodyCol2.innerText = loadedChave.getQuantidade
+        bodyRow.appendChild(bodyCol1)
+        bodyRow.appendChild(bodyCol2)
+        table.appendChild(bodyRow)
+        //console.log(loadedChave.getId)
     }
 }
 
+table.appendChild(headRow)
+
+//nomeTH.innerText = "Chave"
+//quantidadeTH.innerText = "Quantidade"
+
+//table.append(headrow)
+//table.append(bodyrow)
+//bodyrow.append(nomeTH)
+//bodyrow.append(quantidadeTH)
+
+
+document.body.append(table);
+
+const form = document.getElementById("newChaveForm")
+
 form.addEventListener("submit", function e(event){
-    event.preventDefault()
+    //event.preventDefault()
     let newChaveId = 0
     for (let i = 0; localStorage.getItem(i)!=null; i++) {
         newChaveId++
         console.log(i)
     }
+
+
     const newChave = createChave(newChaveId, document.getElementById("nomeForm").value, document.getElementById("quantidadeForm").value)
     const stringChave = JSON.stringify(newChave)
     localStorage.setItem(newChaveId, stringChave)
@@ -55,4 +88,3 @@ function deleteChave(){
 
     //const storedData = localStorage.getItem(0);
     //const loadedStock = storedData ? JSON.parse(storedData) : {};
-    //localStorage.clear()
